5f5f2edc0e7ba4b376d7f22511420dae3fcafd04
jyotimittal1987 <jyotimittal1987@gmail.com>
2022-07-28 12:37:12 +0530

    Add files via upload

---
+++ src/app/fruit-manager/fruit-manager.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Fruit } from '../models/fruit';
+import { FRUITS } from '../models/fruit-list';
+
+@Component({
+  selector: 'app-fruit-manager',
+  templateUrl: './fruit-manager.component.html',
+  styleUrls: ['./fruit-manager.component.css']
+})
+export class FruitManagerComponent implements OnInit {
+  fruits = FRUITS;
+  searchText: string = '';
+  temp: Fruit[] = this.fruits;
+  displayNoRecords: boolean = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  search() {
+    this.fruits = this.fruits.filter((result) =>
+      result?.name?.startsWith(this.searchText)
+    );
+    if (this.fruits.length == 0) {
+      this.displayNoRecords = true;
+    }
+  }
+  reset() {
+    this.searchText = '';
+    this.fruits = [...this.temp];
+    this.displayNoRecords = false;
+
+  }
+
+}
